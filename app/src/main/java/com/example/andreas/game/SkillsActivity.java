cd55a935a3353567ec82ecd35a415b1bf50bf65b
Wiman <Laja911@msn.com>
2017-10-26 11:59:19 +0200

    broken
    Broken code

--- app/src/main/java/com/example/andreas/game/SkillsActivity.java
+++ app/src/main/java/com/example/andreas/game/SkillsActivity.java
@@ -1,22 +1,25 @@
 package com.example.andreas.game;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class SkillsActivity extends AppCompatActivity {
-    int numberOfCheckboxesChecked;
-   // string API = "http://afgamingserver.azurewebsites.net/api/gaming";
+    int numberOfCheckboxesChecked = 0;
+    // string API = "http://afgamingserver.azurewebsites.net/api/gaming";
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_skills);
-        final int checkBoxCount = 0;
+        // final int checkBoxCount = 0;
 
         final CheckBox Konfliktlösningsförmåga = (CheckBox) findViewById(R.id.Konfliktlösningsförmåga);
         final CheckBox Kommunikationsförmåga = (CheckBox) findViewById(R.id.Kommunikationsförmåga);
@@ -30,6 +33,7 @@ public class SkillsActivity extends AppCompatActivity {
         final CheckBox Initiativtagare = (CheckBox) findViewById(R.id.Initiativtagare);
         final CheckBox Ansvarstagande = (CheckBox) findViewById(R.id.Ansvarstagande);
         final CheckBox Ledarskapsförmåga = (CheckBox) findViewById(R.id.Ledarskapsförmåga);
+
 
 
         Button clickMe = (Button) findViewById(R.id.getGame);
@@ -51,29 +55,177 @@ public class SkillsActivity extends AppCompatActivity {
                 OUTPUT.append("First skill:").append(Ledarskapsförmåga.isChecked());
 
                 //Toast.makeText(SkillsActivity.this, OUTPUT.toString(),Toast.LENGTH_LONG).show();
-                if(Ansvarstagande.isChecked() && Initiativtagare.isChecked()){
+                if (Konfliktlösningsförmåga.isChecked() != true){
 
-                   Toast.makeText(SkillsActivity.this,"Cs:Go och LoL",Toast.LENGTH_LONG).show();
-
-
-                }
-
-                if(Tålamod.isChecked() && Konfliktlösningsförmåga.isChecked() || Tålamod.isChecked() || Konfliktlösningsförmåga.isChecked()){
-
-                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar WoW eller LoL",Toast.LENGTH_LONG).show();
-                }
-
-                if (Kommunikationsförmåga.isChecked() && Tålamod.isChecked()){
-                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar Cs:go eller WoW ",Toast.LENGTH_LONG).show();
-
-                }
-
-
-
+                    //Toast.makeText(SkillsActivity.this,"BLÄÄÄ",Toast.LENGTH_LONG).show();
 
 
             }
 
+                if(Ansvarstagande.isChecked() || Initiativtagare.isChecked() || Sammarbetsförmåga.isChecked() ||Fokus.isChecked() ||Ledarskapsförmåga.isChecked() ||
+                                                 Besluttagningförmåga.isChecked() || Flexibel.isChecked() ||
+                                                 Initiativtagare.isChecked()){
+                    if((Kommunikationsförmåga.isChecked() == false)  || (Tålamod.isChecked() == false)){
+
+                        Toast.makeText(SkillsActivity.this,"Cs:Go och LoL",Toast.LENGTH_LONG).show();
+
+                    }
+
+
+                }
+                else if((Tålamod.isChecked() && Konfliktlösningsförmåga.isChecked() || Konfliktlösningsförmåga.isChecked()) && !Tålamod.isChecked()){
+                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar WoW eller LoL",Toast.LENGTH_LONG).show();
+                }
+                else if (Kommunikationsförmåga.isChecked() && Tålamod.isChecked()){
+                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar Cs:go eller WoW ",Toast.LENGTH_LONG).show();
+                }
+                else if(!Kommunikationsförmåga.isChecked()  || !Tålamod.isChecked() || !Konfliktlösningsförmåga.isChecked() || !Ansvarstagande.isChecked() || !Initiativtagare.isChecked() || !Prioriteringsförmåga.isChecked() ){
+                    Toast.makeText(SkillsActivity.this,"Cs:go/WoW/LoL är spel du söker",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+        });
+        Konfliktlösningsförmåga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 3) {
+                    Konfliktlösningsförmåga.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Kommunikationsförmåga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 3) {
+                    Kommunikationsförmåga.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Ansvarstagande.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 3) {
+                    Ansvarstagande.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Initiativtagare.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 3) {
+                    Initiativtagare.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Sammarbetsförmåga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 2) {
+                    Sammarbetsförmåga.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Prioriteringsförmåga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 2) {
+                    Prioriteringsförmåga.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
+        });
+        Tålamod.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && numberOfCheckboxesChecked >= 2) {
+                    Tålamod.setChecked(false);
+                } else {
+                    // the checkbox either got unchecked
+                    // or there are less than 2 other checkboxes checked
+                    // change your counter accordingly
+                    if (isChecked) {
+                        numberOfCheckboxesChecked++;
+                    } else {
+                        numberOfCheckboxesChecked--;
+                    }
+
+                    // now everything is fine and you can do whatever
+                    // checking the checkbox should do here
+                }
+            }
         });
 
        /* final Button button = (Button) findViewById(R.id.Skill1);
@@ -85,8 +237,10 @@ public class SkillsActivity extends AppCompatActivity {
             }
         });
      */
+
+
     }
 
 
 
-    }
+}
