7a5f759a1d28d5248ec1f392590c03b129173feb
Max Danielsson <danmx@wp.ams.se>
2017-10-31 11:56:45 +0100

    addedSwitchImage
    Very serious descrition of how i added a Switch statement to make imagechange possible on the SRes view

--- app/src/main/java/com/example/andreas/game/SResActivity.java
+++ app/src/main/java/com/example/andreas/game/SResActivity.java
@@ -2,6 +2,7 @@ package com.example.andreas.game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -11,6 +12,7 @@ import static com.example.andreas.game.SkillsActivity.textEtt;
 public class SResActivity extends AppCompatActivity {
 
     TextView textView11;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,37 @@ public class SResActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sres);
 
         textView11 = (TextView) findViewById(R.id.textView11);
-        textView11.setText(textEtt);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Bildbild(textEtt);
+        //textView11.setText(textEtt);
+        //imageView.setImageResource(R.drawable.warcraftlogo);
+    }
+
+    public void Bildbild (String textEtt){
+        switch (textEtt){
+            case "WoW":
+                //död kod
+                 imageView.setImageResource(R.drawable.warcraftlogo);
+                // textView11.setText ("Du vill ha en riktig WoW Gamer!");
+                break;
+            case "CS":
+                //död kod
+                 imageView.setImageResource(R.drawable.counterstrikelogo);
+                // textView11.setText ("Du vill ha en riktig CS Gamer!");
+                break;
+            case "LoL":
+                //död kod
+                 imageView.setImageResource(R.drawable.leagueicon);
+                // textView11.setText ("Du vill ha en riktig LoL Gamer!");
+                break;
+            case "Eh":
+                //död kod
+                // imageView.setImageResource(R.drawable.);
+                 textView11.setText ("Du vill ha en riktig Eh ..? Gamer!");
+                break;
+            default:
+                textView11.setText("Du har inte valt något bra spel!!");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/andreas/game/SkillsActivity.java
+++ app/src/main/java/com/example/andreas/game/SkillsActivity.java
@@ -59,36 +59,34 @@ public class SkillsActivity extends AppCompatActivity {
 
                 //Toast.makeText(SkillsActivity.this, OUTPUT.toString(),Toast.LENGTH_LONG).show();
                 if( !Tålamod.isChecked() && !Kommunikationsförmåga.isChecked() && !Konfliktlösningsförmåga.isChecked() && !Ansvarstagande.isChecked() && !Initiativtagare.isChecked() && !Prioriteringsförmåga.isChecked()){
-                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar LoL/Cs/WoW",Toast.LENGTH_LONG).show();
-                    textEtt = "Du letar efter någon som spelar LoL/Cs/WoW";
+                    //Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar LoL/Cs/WoW",Toast.LENGTH_LONG).show();
+                    textEtt = "Eh";//"Du letar efter någon som spelar LoL/Cs/WoW";
                 }
                 if((!Tålamod.isChecked() & !Konfliktlösningsförmåga.isChecked() & !Kommunikationsförmåga.isChecked() &  !Problemlösning.isChecked() & !Prioriteringsförmåga.isChecked()) & (Ansvarstagande.isChecked() | Initiativtagare.isChecked())){
-                    Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar Cs/LoL",Toast.LENGTH_LONG).show();
+                    //Toast.makeText(SkillsActivity.this,"Du letar efter någon som spelar Cs/LoL",Toast.LENGTH_LONG).show();
                     textEtt = "Du letar efter någon som spelar CS/LoL";
                 }
                 if((!Tålamod.isChecked() & !Problemlösning.isChecked() & !Kommunikationsförmåga.isChecked() & !Ansvarstagande.isChecked() & !Initiativtagare.isChecked() & !Ansvarstagande.isChecked()) & (Konfliktlösningsförmåga.isChecked() | (Prioriteringsförmåga.isChecked()))){
-                    Toast.makeText(SkillsActivity.this," Du letar efter någon som spelar WoW/LoL",Toast.LENGTH_LONG).show();
+                    //Toast.makeText(SkillsActivity.this," Du letar efter någon som spelar WoW/LoL",Toast.LENGTH_LONG).show();
                     textEtt = "Du letar efter någon som spelar LoL/WoW";
                 }
                 if(( !Tålamod.isChecked() & !Konfliktlösningsförmåga.isChecked() & !Prioriteringsförmåga.isChecked() & !Ansvarstagande.isChecked() & !Initiativtagare.isChecked()) & (Kommunikationsförmåga.isChecked() | Problemlösning.isChecked())){
-                    Toast.makeText(SkillsActivity.this, "Du letar efter någon som lirar WoW/CS", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(SkillsActivity.this, "Du letar efter någon som lirar WoW/CS", Toast.LENGTH_LONG).show();
                     textEtt = "Du letar efter någon som spelar Cs/WoW";
                 }
                 if((!Ansvarstagande.isChecked() & !Initiativtagare.isChecked() & !Konfliktlösningsförmåga.isChecked() & !Prioriteringsförmåga.isChecked() & !Kommunikationsförmåga.isChecked() & !Problemlösning.isChecked()) & (Tålamod.isChecked())){
-                    Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar WoW", Toast.LENGTH_LONG).show();
-                    textEtt = "Du letar efter någon som spelar WoW";
+                    //Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar WoW", Toast.LENGTH_LONG).show();
+                    textEtt = "WoW";
                 }
 
                 if((!Initiativtagare.isChecked() & !Konfliktlösningsförmåga.isChecked() & !Prioriteringsförmåga.isChecked() & !Tålamod.isChecked()) & (Problemlösning.isChecked() | Ansvarstagande.isChecked())  | (Initiativtagare.isChecked() & Kommunikationsförmåga.isChecked())){
-                    Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar CS" +
-                            "", Toast.LENGTH_LONG).show();
-                    textEtt = "Du letar efter någon som spelar CS";
+                    //Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar CS" +"", Toast.LENGTH_LONG).show();
+                    textEtt = "CS";
                 }
 
                 if((!Tålamod.isChecked() & !Kommunikationsförmåga.isChecked() & !Problemlösning.isChecked() ) & ( Ansvarstagande.isChecked() & Prioriteringsförmåga.isChecked()) | (Konfliktlösningsförmåga.isChecked() & Ansvarstagande.isChecked())){
-                    Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar LoL" +
-                            "", Toast.LENGTH_LONG).show();
-                    textEtt = "Du letar efter någon som spelar LoL";
+                    //Toast.makeText(SkillsActivity.this, "Du letar efter någon som spelar LoL" +"", Toast.LENGTH_LONG).show();
+                    textEtt = "LoL";
                 }
 
                
