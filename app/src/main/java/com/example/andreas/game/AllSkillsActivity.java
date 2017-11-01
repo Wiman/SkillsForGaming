8e9a327ba5a2da308980048df845a4b131ebc294
Wiman <Laja911@msn.com>
2017-11-01 14:14:16 +0100

    test

--- app/src/main/java/com/example/andreas/game/AllSkillsActivity.java
+++ app/src/main/java/com/example/andreas/game/AllSkillsActivity.java
@@ -8,6 +8,6 @@ public class AllSkillsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_skills2);
+        setContentView(R.layout.activity_all_skills);
     }
 }
