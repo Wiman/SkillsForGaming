085e80b8bb5b3edbd3419e070a4ac4a49a8e2f5c
Max Danielsson <danmx@wp.ams.se>
2017-11-01 15:02:33 +0100

    FixFixy

--- app/src/main/java/com/example/andreas/game/AllSkillsActivity.java
+++ app/src/main/java/com/example/andreas/game/AllSkillsActivity.java
@@ -2,12 +2,19 @@ package com.example.andreas.game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AllSkillsActivity extends AppCompatActivity {
+
+    TextView textview12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_skills);
+
+        textview12 = (TextView) findViewById(R.id.textView12);
+
+        textview12.setText("Placeholder, is gon depend on what button");
     }
 }
